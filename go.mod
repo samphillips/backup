09db81246452defb5c64240aace9ae482fd248c2
Sam Phillips <samphillips0312@gmail.com>
2020-06-16 08:37:38 +0100

    Use the gocheck module

--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/cheggaaa/pb v2.0.7+incompatible
 	github.com/cheggaaa/pb/v3 v3.0.4
 	github.com/jpillora/opts v1.2.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/withmandala/go-log v0.1.0
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
 	gopkg.in/VividCortex/ewma.v1 v1.1.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f
 	gopkg.in/cheggaaa/pb.v2 v2.0.7 // indirect
 	gopkg.in/fatih/color.v1 v1.7.0 // indirect
 	gopkg.in/mattn/go-colorable.v0 v0.1.0 // indirect
--- internal/file/file_test.go
+++ internal/file/file_test.go
@@ -1,14 +1,14 @@
 package file
 
 import (
-	"bytes"
 	"crypto/md5"
 	"encoding/hex"
 	"os"
 	"path/filepath"
-	"reflect"
 	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
 
 type MockFileInfo struct {
@@ -62,153 +62,116 @@ func createFile(path string, data []byte) error {
 	return nil
 }
 
-func TestCopyFileCopiesFile(t *testing.T) {
+func Test(t *testing.T) { TestingT(t) }
+
+type FileTestSuite struct{}
+
+var _ = Suite(&FileTestSuite{})
+
+func (*FileTestSuite) TestCopyFileCopiesFile(c *C) {
 	baseDir := os.TempDir()
 	srcFile := filepath.Join(baseDir, "src")
 	srcBytes := []byte{'t', 'e', 's', 't'}
 
 	err := createFile(srcFile, srcBytes)
-	if err != nil {
-		t.Errorf("Error creating source file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(srcFile)
 
 	dstFile := filepath.Join(baseDir, "dst")
 	err = CopyFile(srcFile, dstFile)
-	if err != nil {
-		t.Errorf("Error copying source file to destination: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(dstFile)
 
 	src, err := os.Open(srcFile)
-	if err != nil {
-		t.Errorf("Error opening source file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer src.Close()
 	var srcData []byte
 	_, err = src.Read(srcData)
-	if err != nil {
-		t.Errorf("Error reading source file: %s", err)
-	}
+	c.Check(err, IsNil)
 
 	dst, err := os.Open(dstFile)
-	if err != nil {
-		t.Errorf("Error opening destination file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer dst.Close()
 	var dstData []byte
 	_, err = dst.Read(dstData)
-	if err != nil {
-		t.Errorf("Error reading destination file: %s", err)
-	}
+	c.Check(err, IsNil)
 
-	if bytes.Compare(srcData, dstData) != 0 {
-		t.Errorf("Error: source and destination file contents not equal")
-	}
+	c.Check(srcData, DeepEquals, dstData)
 }
 
-func TestCopyFileErrorsOnNonExistentSourceFile(t *testing.T) {
+func (*FileTestSuite) TestCopyFileErrorsOnNonExistentSourceFile(c *C) {
 	baseDir := os.TempDir()
 	srcFile := filepath.Join(baseDir, "src")
 	dstFile := filepath.Join(baseDir, "dst")
 
 	err := CopyFile(srcFile, dstFile)
 
-	if err == nil {
-		t.Errorf("CopyFile did not return an error")
-	}
+	c.Check(err, Not(IsNil))
 }
 
-func TestCopyFileOverwritesExistingDst(t *testing.T) {
+func (*FileTestSuite) TestCopyFileOverwritesExistingDst(c *C) {
 	baseDir := os.TempDir()
 	srcFile := filepath.Join(baseDir, "src")
 	srcBytes := []byte{'t', 'e', 's', 't'}
 
 	err := createFile(srcFile, srcBytes)
-	if err != nil {
-		t.Errorf("Error creating source file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(srcFile)
 
 	dstFile := filepath.Join(baseDir, "dst")
 	dstBytes := []byte{'t', 'e'}
 
 	err = createFile(dstFile, dstBytes)
-	if err != nil {
-		t.Errorf("Error creating destination file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(dstFile)
 
 	err = CopyFile(srcFile, dstFile)
-
-	if err != nil {
-		t.Errorf("Error copying source file to destination: %s", err)
-	}
+	c.Check(err, IsNil)
 
 	src, err := os.Open(srcFile)
-	if err != nil {
-		t.Errorf("Error opening source file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer src.Close()
 	var srcData []byte
 	_, err = src.Read(srcData)
-	if err != nil {
-		t.Errorf("Error reading source file: %s", err)
-	}
+	c.Check(err, IsNil)
 
 	dst, err := os.Open(dstFile)
-	if err != nil {
-		t.Errorf("Error opening destination file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer dst.Close()
 	var dstData []byte
 	_, err = dst.Read(dstData)
-	if err != nil {
-		t.Errorf("Error reading destination file: %s", err)
-	}
+	c.Check(err, IsNil)
 
-	if bytes.Compare(srcData, dstData) != 0 {
-		t.Errorf("Error: source and destination file contents not equal")
-	}
+	c.Check(srcData, DeepEquals, dstData)
 }
 
-func TestHashFileCreatesCorrectHash(t *testing.T) {
+func (*FileTestSuite) TestHashFileCreatesCorrectHash(c *C) {
 	baseDir := os.TempDir()
 	srcFile := filepath.Join(baseDir, "src")
 	srcBytes := []byte{'t', 'e', 's', 't'}
 
 	err := createFile(srcFile, srcBytes)
-	if err != nil {
-		t.Errorf("Error creating source file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(srcFile)
 
 	hashBytes := md5.Sum(srcBytes)
 	hashString := hex.EncodeToString(hashBytes[:])
 
 	testHashString, err := hashFile(srcFile)
+	c.Check(err, IsNil)
 
-	if err != nil {
-		t.Errorf("hashFile returned an error: %s", err)
-	}
-
-	if hashString != testHashString {
-		t.Errorf("Data hash %s not equal to file hash %s", hashString, testHashString)
-	}
+	c.Check(hashString, Equals, testHashString)
 }
 
-func TestHashFileReturnsErrorIfFileDoesNotExist(t *testing.T) {
+func (*FileTestSuite) TestHashFileReturnsErrorIfFileDoesNotExist(c *C) {
 	baseDir := os.TempDir()
 	srcFile := filepath.Join(baseDir, "src")
 
 	_, err := hashFile(srcFile)
-
-	if err == nil {
-		t.Errorf("hashFile did not return an error")
-	}
+	c.Check(err, Not(IsNil))
 }
 
-func TestGenerateBackupDetailsAddsDirectoriesNotInBackupLocation(t *testing.T) {
+func (*FileTestSuite) TestGenerateBackupDetailsAddsDirectoriesNotInBackupLocation(c *C) {
 	srcIndex := map[string]os.FileInfo{
 		"dir1": &MockFileInfo{
 			name:    "dir1",
@@ -233,29 +196,16 @@ func TestGenerateBackupDetailsAddsDirectoriesNotInBackupLocation(t *testing.T) {
 
 	files, directories, symlinks := GenerateBackupDetails(srcIndex, dstIndex, srcDir, dstDir)
 
-	if len(files) != 0 {
-		t.Errorf("List of files is not none")
-	}
-
-	if len(symlinks) != 0 {
-		t.Errorf("List of symlinks is not none")
-	}
-
-	if len(directories) != 2 {
-		t.Errorf("List of directories should be 2")
-	}
-
-	expectedDirs := []string{
+	c.Check(files, HasLen, 0)
+	c.Check(symlinks, HasLen, 0)
+	c.Check(directories, HasLen, 2)
+	c.Check(directories, DeepEquals, []string{
 		"dir1",
 		"dir2",
-	}
-
-	if reflect.DeepEqual(directories, expectedDirs) {
-		t.Errorf("directories contents does not match expected")
-	}
+	})
 }
 
-func TestGenerateBackupDetailsAddsFilesNotInBackupLocation(t *testing.T) {
+func (*FileTestSuite) TestGenerateBackupDetailsAddsFilesNotInBackupLocation(c *C) {
 	srcIndex := map[string]os.FileInfo{
 		"file1": &MockFileInfo{
 			name:    "file1",
@@ -280,50 +230,31 @@ func TestGenerateBackupDetailsAddsFilesNotInBackupLocation(t *testing.T) {
 
 	files, directories, symlinks := GenerateBackupDetails(srcIndex, dstIndex, srcDir, dstDir)
 
-	if len(files) != 2 {
-		t.Errorf("List of files should be sized 2")
-	}
-
-	expectedFiles := []string{
+	c.Check(files, HasLen, 2)
+	c.Check(symlinks, HasLen, 0)
+	c.Check(directories, HasLen, 0)
+	c.Check(files, DeepEquals, []string{
 		"file1",
 		"file2",
-	}
-
-	if reflect.DeepEqual(files, expectedFiles) {
-		t.Errorf("files contents does not match expected")
-	}
-
-	if len(symlinks) != 0 {
-		t.Errorf("List of symlinks is not none")
-	}
-
-	if len(directories) != 0 {
-		t.Errorf("List of directories is not none")
-	}
+	})
 }
 
-func TestGenerateBackupDetailsAddsSymlinksNotInBackupLocation(t *testing.T) {
+func (*FileTestSuite) TestGenerateBackupDetailsAddsSymlinksNotInBackupLocation(c *C) {
 	baseDir := os.TempDir()
 	targetFile := filepath.Join(baseDir, "target")
 
 	err := createFile(targetFile, []byte{})
-	if err != nil {
-		t.Errorf("Error creating file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(targetFile)
 
 	symlink1 := filepath.Join(baseDir, "symlink1")
 	err = os.Symlink(targetFile, symlink1)
-	if err != nil {
-		t.Errorf("Error creating file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(symlink1)
 
 	symlink2 := filepath.Join(baseDir, "symlink2")
 	err = os.Symlink(targetFile, symlink2)
-	if err != nil {
-		t.Errorf("Error creating file: %s", err)
-	}
+	c.Check(err, IsNil)
 	defer os.Remove(symlink2)
 
 	srcIndex := map[string]os.FileInfo{
@@ -350,24 +281,11 @@ func TestGenerateBackupDetailsAddsSymlinksNotInBackupLocation(t *testing.T) {
 
 	files, directories, symlinks := GenerateBackupDetails(srcIndex, dstIndex, srcDir, dstDir)
 
-	if len(symlinks) != 2 {
-		t.Errorf("List of symlinks should be of size 2")
-	}
-
-	expectedSymlinks := map[string]string{
-		"symlink1": targetFile,
-		"symlink2": targetFile,
-	}
-
-	if reflect.DeepEqual(symlinks, expectedSymlinks) {
-		t.Errorf("symlinks contents does not match expected")
-	}
-
-	if len(files) != 0 {
-		t.Errorf("List of files is not none")
-	}
-
-	if len(directories) != 0 {
-		t.Errorf("List of directories is not none")
-	}
+	c.Check(files, HasLen, 0)
+	c.Check(symlinks, HasLen, 2)
+	c.Check(directories, HasLen, 0)
+	c.Check(symlinks, DeepEquals, map[string]string{
+		"symlink1": "target",
+		"symlink2": "target",
+	})
 }
